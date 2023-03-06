75283ad6621352c5ebef41aa175003a3bc63d4eb
v203 <vuyisamr203@gmail.com>
2023-03-06 13:22:52 +0200

    reverted base config IV

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/804-cosmetics"
+  base:"/804-cosmetics/"
   
 })
